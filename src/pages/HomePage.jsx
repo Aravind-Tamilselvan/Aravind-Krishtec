ad7fe772aea09fd977027f885de117a6584cc6eb
Aravind-Tamilselvan <aravindtamilselvan7@gmail.com>
2025-02-21 19:01:27 +0530

    New Commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,21 +8,21 @@ import { FaInstagram } from "react-icons/fa";
 const HomePage = () => {
     return (
         <>
-            <div class="home" id="home">
-                <img src='/ProfileImg.jpg' alt="Profile picture" class="profile-img"/>
+            <div className="home" id="home">
+                <img src='/ProfileImg.jpg' alt="Profile picture" className="profile-img"/>
                     <div>
-                        <div class="content">
+                        <div className="content">
                             <h3>Hello,myself</h3>
                             <h1>Aravind T</h1>
                             <h3>I'm a Frontend Developer</h3>
                             <p>A  B.Tech Artificial Intelligence and Data Science student interested in building web applications that solve real world problems</p>
                         </div>
-                        <div class="icons">
-                            <a class="bttn" href="https://www.linkedin.com/in/aravind-t-3506a8228/"><FaLinkedinIn/></a>
-                            <a href="#" class="bttn"><LuGithub /></a>
-                            <a href="https://www.instagram.com/aravind__privt/" class="bttn"><FaInstagram/></a>
+                        <div className="icons">
+                            <a className="bttn" href="https://www.linkedin.com/in/aravind-t-3506a8228/"><FaLinkedinIn/></a>
+                            <a href="#" className="bttn"><LuGithub /></a>
+                            <a href="https://www.instagram.com/aravind__privt/" className="bttn"><FaInstagram/></a>
 
-                            <a href="/Aravind.pdf" download="Aravind.pdf"><button class="btn">Download CV</button></a>
+                            <a href="/Aravind.pdf" download="Aravind.pdf"><button className="btn">Download CV</button></a>
                         </div>
                     </div>
             </div>
