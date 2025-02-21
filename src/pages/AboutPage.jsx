5768ee92eda7f46866dcfea65e2028e116de9ad5
Aravind-Tamilselvan <aravindtamilselvan7@gmail.com>
2025-02-21 18:56:15 +0530

    changed About

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
         <div>
             <h1>About Me</h1>
             <h3>I'm a B.Tech AI&DS student</h3>
-            <p id="about-me">I am an AI and Data Science student with strong coding skills in Python, SQL, and JavaScript. Passionate about web development, I specialize in the MERN stack, building dynamic and user-friendly applications. As a football player, I developed key soft skills like teamwork, persistence, and leadership. Additionally, I have experience in copywriting for major travel brands and mentoring students for job placements, blending technical expertise with communication and guidance skills.</p>
+            <p id="about-me">I am an AI and Data Science student with strong coding skills in Python, SQL, and JavaScript. Passionate about web development, I specialize in the MERN stack, building dynamic and user-friendly applications. As a football player, I developed key soft skills like teamwork, persistence, and leadership.</p>
 
         </div>
 
